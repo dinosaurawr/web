dcc8174276933e8138c85f306f225d84e94bed6f
Alexander <dinosaurawr.1@gmail.com>
2018-10-30 19:50:07 +0300

    updated init script

--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf
 sudo /etc/init.d/gunicorn restart
