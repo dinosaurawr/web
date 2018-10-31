8ef9468bb730041b1d2c3f7b49dc9f5a4cac4266
Alexander <dinosaurawr.1@gmail.com>
2018-10-31 16:05:17 +0300

    Update ask/ask/urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from qa import views
 
 urlpatterns = [
-    url(r'^$', qa.views.test),
-    url(r'^login/$', qa.views.test),
-    url(r'^signup/$', qa.views.test),
-    url(r'^question/(\d+)/$', qa.views.test),
-    url(r'^ask/$', qa.views.test),
-    url(r'^popular/$', qa.views.test),
-    url(r'^new/$', qa.views.test),
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/(\d+)/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
     url(r'^admin/$', admin.site.urls),
 ]
