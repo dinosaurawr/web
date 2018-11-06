2748bd1b1dcd861e7034e6d9ea3331b5b6d2a434
Alexander <dinosaurawr.1@gmail.com>
2018-11-06 15:21:46 +0300

    django models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
+from  django.contrib.auth.models import User
 
 from django.db import models
 
-# Create your models here.
+def QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+def Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default = 0)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User)
+
+
+
+def Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, null=False, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
